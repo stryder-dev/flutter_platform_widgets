b084dd8f17e16f450c960a80b1730e4656ad44a4
vhschlenker <git@vhschlenker.de>
2018-10-13 17:37:24 +0200

    Add example application

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# flutter_platform_widgets_example
+
+Demonstrates how to use the flutter_platform_widgets plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ example/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
