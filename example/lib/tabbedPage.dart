3d8fd77099d9898744c50b8d4f9db015a29506ef
Lance Johnstone <weblance@outlook.com>
2019-01-25 22:36:16 +1300

    Extended example to add bottom padding

--- example/lib/tabbedPage.dart
+++ example/lib/tabbedPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart' show CupertinoIcons;
-import 'package:flutter/material.dart' show Icons;
+import 'package:flutter/material.dart' show Icons, Colors;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
@@ -15,37 +15,105 @@ class TabbedPageState extends State<TabbedPage> {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    print('Bottom Pad1 ${mediaQuery.padding.top}');
+    print('Bottom Pad2 ${mediaQuery.padding.bottom}');
     return PlatformScaffold(
-        iosContentPadding: true,
-        appBar: PlatformAppBar(
-          title: Text('Platform Widgets'),
-        ),
-        bottomNavBar: PlatformNavBar(
-          currentIndex: _selectedTabIndex,
-          itemChanged: (index) => setState(
-                () {
-                  _selectedTabIndex = index;
-                },
-              ),
-          items: [
-            BottomNavigationBarItem(
-              title: Text('Flag'),
-              icon: PlatformWidget(
-                ios: (_) => Icon(CupertinoIcons.flag),
-                android: (_) => Icon(Icons.flag),
-              ),
+      iosContentPadding: true,
+      appBar: PlatformAppBar(
+        title: Text('Platform Widgets'),
+      ),
+      bottomNavBar: PlatformNavBar(
+        currentIndex: _selectedTabIndex,
+        itemChanged: (index) => setState(
+              () {
+                _selectedTabIndex = index;
+              },
             ),
-            BottomNavigationBarItem(
-              title: Text('Book'),
-              icon: PlatformWidget(
-                ios: (_) => Icon(CupertinoIcons.book),
-                android: (_) => Icon(Icons.book),
-              ),
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Flag'),
+            icon: PlatformWidget(
+              ios: (_) => Icon(CupertinoIcons.flag),
+              android: (_) => Icon(Icons.flag),
+            ),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Book'),
+            icon: PlatformWidget(
+              ios: (_) => Icon(CupertinoIcons.book),
+              android: (_) => Icon(Icons.book),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.limeAccent,
+              alignment: Alignment.center,
+              child: Text('Content 1'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.amberAccent,
+              alignment: Alignment.center,
+              child: Text('Content 2'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.pinkAccent,
+              alignment: Alignment.center,
+              child: Text('Content 3'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.purpleAccent,
+              alignment: Alignment.center,
+              child: Text('Content 4'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.deepPurpleAccent,
+              alignment: Alignment.center,
+              child: Text('Content 5'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.indigoAccent,
+              alignment: Alignment.center,
+              child: Text('Content 6'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.blueAccent,
+              alignment: Alignment.center,
+              child: Text('Content 7'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.lightBlueAccent,
+              alignment: Alignment.center,
+              child: Text('Content 8'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(36),
+              color: Colors.tealAccent,
+              alignment: Alignment.center,
+              child: Text('Content 9'),
+            ),
+            PlatformWidget(
+              ios: (_) => Container(
+                    padding:
+                        EdgeInsets.only(bottom: 50 + mediaQuery.padding.bottom),
+                  ),
+              android: (_) => Container(),
             ),
           ],
         ),
-        body: Center(
-          child: Text('Tabbed Page'),
-        ));
+      ),
+    );
   }
 }
