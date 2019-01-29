14d31f267d01b5b95890ed481cb186d492242153
Lance Johnstone <weblance@outlook.com>
2019-01-29 22:24:41 +1300

    Add iosContentBottomPadding, optional PlatformWidget args

--- lib/src/platform_widget.dart
+++ lib/src/platform_widget.dart
@@ -12,18 +12,23 @@ class PlatformWidget extends PlatformWidgetBase<Widget, Widget> {
   final PlatformBuilder<Widget> android;
   final PlatformBuilder<Widget> ios;
 
-  PlatformWidget({Key key, this.ios, this.android}) : super(key: key) {
-    assert(android != null);
-    assert(ios != null);
-  }
+  PlatformWidget({Key key, this.ios, this.android}) : super(key: key);
 
   @override
   Widget createAndroidWidget(BuildContext context) {
-    return android(context);
+    if (android == null) {
+      return Container();
+    } else {
+      return android(context);
+    }
   }
 
   @override
   Widget createIosWidget(BuildContext context) {
-    return ios(context);
+    if (ios == null) {
+      return Container();
+    } else {
+      return ios(context);
+    }
   }
 }
