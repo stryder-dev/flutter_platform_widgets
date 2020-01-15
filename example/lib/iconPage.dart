9d716cc7ee24ba95c07cbd952acf98c5bbda3a03
Lance Johnstone <weblance@outlook.com>
2020-01-16 12:33:00 +1300

    Fix PlatformTabbedScaffold

--- example/lib/iconPage.dart
+++ example/lib/iconPage.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-// TODO have solid/filled for both, have normal which could be outline ios, filled android
-
 class IconsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
