629f30d719d416fd4ec0cf372554bae38c0c9af8
Lance Johnstone <lancejohnstone@Lances-MBP.fritz.box>
2023-05-30 18:18:30 +1200

    Fix dialog action button padding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
+## [3.3.5]
+
+#### (Flutter version support: v3.7.0 - 3.7.12)
+
+- Fix material dialog action button padding. Will use default padding
+
 ## [3.3.4]
 
 #### (Flutter version support: v3.7.0 - 3.7.12)
 
--- Fix for PlatformApp not using the correct theme data in dark mode
+- Fix for PlatformApp not using the correct theme data in dark mode
 
 ## [3.3.3]
 
--- lib/src/platform_alert_dialog.dart
+++ lib/src/platform_alert_dialog.dart
@@ -131,8 +131,7 @@ class PlatformAlertDialog extends PlatformWidgetBase<Widget, AlertDialog> {
       key: data?.widgetKey ?? widgetKey,
       actions: data?.actions ?? actions,
       content: data?.content ?? content,
-      contentPadding: data?.contentPadding ??
-          const EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 24.0),
+      contentPadding: data?.contentPadding,
       semanticLabel: data?.semanticLabel,
       title: data?.title ?? title,
       titlePadding: data?.titlePadding,
@@ -143,7 +142,7 @@ class PlatformAlertDialog extends PlatformWidgetBase<Widget, AlertDialog> {
       titleTextStyle: data?.titleTextStyle,
       scrollable: data?.scrollable ?? false,
       actionsOverflowDirection: data?.actionsOverflowDirection,
-      actionsPadding: data?.actionsPadding ?? EdgeInsets.zero,
+      actionsPadding: data?.actionsPadding,
       buttonPadding: data?.buttonPadding,
       actionsOverflowButtonSpacing: data?.actionsOverflowButtonSpacing,
       clipBehavior: data?.clipBehavior ?? Clip.none,
