c424a3b9cd9c29fc4f2d871f9b6f689936d5641a
Ahmed Hnewa <73608287+freshtechtips@users.noreply.github.com>
2023-10-20 20:49:37 +0300

    Follow name convenstion

--- lib/src/extensions/build_context.dart
+++ lib/src/extensions/build_context.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart' show BuildContext;
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-extension BuildContextExtensions on BuildContext {
+extension BuildContextExt on BuildContext {
   bool get isMaterialDesign => isMaterial(this);
   bool get isCupertinoDesign => isCupertino(this);
 }
